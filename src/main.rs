eaa00916627fd12af944955a38836cc46f16e6a6
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-03 01:43:37 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+// Bevy code commonly triggers these lints and they may be important signals
+// about code quality. They are sometimes hard to avoid though, and the CI
+// workflow treats them as errors, so this allows them throughout the project.
+// Feel free to delete this line.
+#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Hold The Line".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        ..Default::default()
+    });
+}
